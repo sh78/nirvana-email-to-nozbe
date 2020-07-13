7570b5cf8021cad774d573f61dc2c13d15395fc5
Sean Henderson <mail@sean.sh>
2020-07-13 15:43:10 -0700

    Commit 0
    - npm init
    - JavaScript Standard Style via ESLint
    - Git ignore from github.com/github

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es2020": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "extends": "standard",
+    "rules": {
+    }
+};
